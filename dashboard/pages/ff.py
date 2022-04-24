3e019c9c9d257cad53bde5069aaebe2928316f9c
Seth Pate <swpate@icloud.com>
2022-04-24 11:59:52 -0400

    ff layer skeleton

--- dashboard/pages/ff.py
+++ dashboard/pages/ff.py
@@ -1,11 +1,27 @@
 from dash import html
+from dash import dcc
+
+explain = dcc.Markdown('''
+    ## purpose of a feedforward layer
+
+    Nonlinearities. See example of transform and relu. What is a relu?
+
+    ## layer norm
+
+    Transform the outputs of a certain layer to mean 0 and std 1.
+
+    Layer normalization is an alternative to batch normalization.
+
+    ## scaling
+
+    Scaling is meant to 'stabilize' training by reducing the
+    'temperature' of the softmax.
+    ''')
 
 def get_layout():
     layout = html.Div([
         html.H1("Feedforward", id="ff-section"),
         html.Hr(),
-        html.P("And how about those MLPs, huh?"),
-        html.Hr(),
-        html.P("whole buncha science"),
+        explain,
         ])
     return layout
