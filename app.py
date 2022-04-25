86a4f871c346b08759a780a040077bf5aa4a1c5f
Seth Pate <swpate@icloud.com>
2022-04-25 18:58:53 -0400

    app server

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import dash_bootstrap_components as dbc
 from maindash import app
 from pages.single import make_single_layout
 
+server = app.server
+
 if __name__ == "__main__":
     app.layout = make_single_layout()
     app.run_server(debug=True)
