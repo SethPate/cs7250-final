c462983e6e60b6c461f0af7b9084719d56b98e4b
Seth Pate <swpate@icloud.com>
2022-04-24 23:08:38 -0400

    better data layers, ff layout

--- dashboard/pages/ff.py
+++ dashboard/pages/ff.py
@@ -5,46 +5,44 @@ from maindash import app
 from dash.dependencies import Input,Output
 from utils.functions import matrix_fig
 
-explain = dcc.Markdown('''
-    ## purpose of a feedforward layer
-
-    Nonlinearities. See example of transform and relu. What is a relu?
-    - show unbalanced 'before' transformation
-    - show unbalanced 'after' transformation
-
-    ## residual connections
-    - show two layers normally
-    - show three layers residually
-
-    ## layer norm
-
-    Transform the outputs of a certain layer to mean 0 and std 1.
-    Layer normalization is an alternative to batch normalization.
-
-    - show 'after'
-    - show post batch norm
-    - show post layer norm
-
-    ''')
-
 def get_layout(params):
     layout = html.Div([
         html.H1("Feedforward", id="ff-section"),
         html.Hr(),
-        explain,
-        html.Div(id='ff-figure'),
+        dcc.Markdown('''
+        ## purpose of a feedforward layer
+
+        Nonlinearities. See example of transform and relu. What is a relu?
+        - show unbalanced 'before' transformation
+        - show unbalanced 'after' transformation
+        '''),
+        html.Div(id='after-linear'),
+        dcc.Markdown('''
+        ## residual connections
+        - show two layers normally
+        - show three layers residually
+        '''),
+        dcc.Markdown('''
+        ## layer norm
+
+        Transform the outputs of a certain layer to mean 0 and std 1.
+        Layer normalization is an alternative to batch normalization.
+
+        - show 'after'
+        - show post batch norm
+        - show post layer norm
+        ''')
         ])
     return layout
 
-def make_ff_fig(params):
-    ix = params['current_sample_ix']
-    ff = params['layerdata'][ix]['linear_1'] # np.array(t,d)
-    return matrix_fig(ff, "feedforward output")
 
-@app.callback(Output("ff-figure","children"),
+
+@app.callback(Output("after-linear","children"),
             Input("datastore","data"))
-def update_ff(data):
-    if not data:
+def update_after_linear(params):
+    if not params:
         return
     else:
-        return make_ff_fig(data)
+        ix = params['current_sample_ix']
+        linear = params['layerdata'][ix]['linear'] # np.array(t,d)
+        return matrix_fig(linear, "feedforward output")
