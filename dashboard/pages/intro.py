eb79d56cf695d75eafb5e0830dba739ec2c5d17f
Alberto Mario Ceballos-Arroyo <alceballosa@unal.edu.co>
2022-04-25 02:06:08 -0400

    Fix dropdown

--- dashboard/pages/intro.py
+++ dashboard/pages/intro.py
@@ -1,6 +1,10 @@
-from dash import dcc, html
-from dash.dependencies import Input,Output,State
+from dash import dcc
+from dash import html
+from dash.dependencies import Input
+from dash.dependencies import Output
+from dash.dependencies import State
 from maindash import app
+
 
 def make_dropdown(params):
     ix = params["current_sample_ix"]
@@ -17,23 +21,27 @@ def make_layout(params):
 
     return html.Div(
         [
-            dcc.Markdown('''
+            dcc.Markdown(
+                """
             Transformer networks are the basic technology powering many
             new AI products, including:
             * [Google Pathways Language Model](https://ai.googleblog.com/2022/04/pathways-language-model-palm-scaling-to.html), which explain bad jokes about TPU 'pods',
             * arst
-            '''),
-            html.Div(id='show-sample'),
+            """
+            ),
+            make_dropdown(params),
+            html.Div(id="show-sample"),
         ],
         id="home",
     )
 
-@app.callback(Output("show-sample","children"),
-        Input("datastore","data"))
+
+@app.callback(Output("show-sample", "children"), Input("datastore", "data"))
 def update_sample(params):
     if not params:
         return
     else:
         ix = params["current_sample_ix"]
         sample = params["layerdata"][ix]["sample"]
-        return html.P(sample)
+        print(sample)
+        return html.P(" ".join(sample))
