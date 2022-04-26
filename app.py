dfb5632501caf0c660917f51a331d90b844c71ce
Seth Pate <swpate@icloud.com>
2022-04-25 23:04:14 -0400

    heroku bug

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from maindash import app
 from pages.single import make_single_layout
 
 server = app.server
+app.layout = make_single_layout()
 
 if __name__ == "__main__":
-    app.layout = make_single_layout()
     app.run_server(debug=True)
